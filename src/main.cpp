0311c1f5edbb1e207b9af354220235ac4e58aa28
Sylvain Boyer <sylvain.boyer@npl.co.uk>
2021-01-25 18:39:19 +0000

    Leaky integrator instead of "moving average"

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #define MIN_DUTY_CYCLE 0
 #define MAX_DUTY_CYCLE 100
 #define DEFAULT_DUTY_CYCLE 20
-
+#define RPM_READING_TIMEOUT 150  //in ms will set fan speed to 0 if timeout
 
 // fan command
 float dutyCycle = DEFAULT_DUTY_CYCLE;
@@ -26,7 +26,7 @@ volatile boolean flag_fanPeriod = false;
 
 float instantFanSpeed = 0;
 float averageFanSpeed = 0; // for moving average
-float  alpha = 0.25;
+float  alpha = 0.8;//0.8; // 0 = no filter 1 = filter with 0 Hz frequency cut
 
 
 
@@ -70,16 +70,18 @@ void loop()
     Timer1.pwm(FAN_PWM_PIN, (dutyCycle / 100) * 1023);
 
     Serial.println(getFanSpeed());
+    //getFanSpeed();
+    //Serial.println(100*abs(averageFanSpeed-instantFanSpeed)/averageFanSpeed);  
 
-    //Diplay RPM readings
-   /** if (flag_fanPeriod == 1){
+    //DUBUGING Diplay RPM readings
+    /**if (flag_fanPeriod == 1){
       Serial.print("fan period: ");Serial.print(fanPeriod);Serial.println("  us");
       //Serial.print("fan frequency: ");Serial.print((float) 1000000/ (PULSE_PER_REV * fanPeriod));Serial.println("  Hz");
       //Serial.print("fan speed: ");Serial.print((float) 60*1000000/ (PULSE_PER_REV * fanPeriod));Serial.println("  RPM");
       flag_fanPeriod = false;
     }*/
 
-    delay(10);
+    //delay(10);
 }
 
 
@@ -87,17 +89,25 @@ void loop()
 
 float getFanSpeed(void) {
   long timestamp = millis();
-  while ((millis() - timestamp) < 150 && (flag_fanPeriod == false)) ; //wait for both timeout and update
+  while ((millis() - timestamp) < RPM_READING_TIMEOUT && (flag_fanPeriod == false)) ; //wait for both timeout and update
   //timeout, check update
   if (flag_fanPeriod == 1)  {
     instantFanSpeed = 60*1000000/ (PULSE_PER_REV * fanPeriod);
+    
+    /**if(instantFanSpeed > 500 && averageFanSpeed > 100 && (100*abs(averageFanSpeed-instantFanSpeed)/averageFanSpeed)>50.0)
+    {
+      instantFanSpeed = averageFanSpeed;
+    }*/
+    // Does not work together with the leaky integrator 
+
     //averageFanSpeed = instantFanSpeed;
-    // moving average   
-    averageFanSpeed = (1-alpha) * averageFanSpeed + alpha * instantFanSpeed;
+
+    // Leaky integrator to smooth data (= low pass filter alpha=0 no filter alpha = 1 all data are filtered)
+    averageFanSpeed = alpha * averageFanSpeed + (1-alpha) *  instantFanSpeed;
     flag_fanPeriod = false;
   } 
   else{
-    averageFanSpeed = 0.0;
+    averageFanSpeed =  0.0;
   } 
   return averageFanSpeed;
 }
